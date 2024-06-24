f6ca917fa18356b289e5a99788965a5f3821501c
sornmonika <monika.sorn@institute.pse.ngo>
2024-06-24 13:22:00 +0700

    layout form

---
+++ index.js
@@ -0,0 +1,7 @@
+// var input = document.getElementById("searchTxt");
+
+// function searchURL() {
+//      window.location = "http://www.myurl.com/search/" + input.value;
+// }
+
+// console.log(event.target['searchTxt'].value);
